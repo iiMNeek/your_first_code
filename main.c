d61085bb1d0ae896f919b39ecddbddf3f71afd34
iiMNeek <50498575+iiMNeek@users.noreply.github.com>
2019-05-10 11:42:14 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-int main()
-{
+int i;
+
   int i;
   for (i = 0; i < 10; i++)
   {
@@ -7,3 +7,48 @@ int main()
   }
   return 0;
 }
+ 35  main.c 
+@@ -1,9 +1,32 @@
+int main()
+{
+	  int i;
+	  for (i = 0; i < 10; i++)
+	  {
+	    printf("Holberton\n");
+	    printf("Hello Holberton\n");
+	  }
+	  return 0;
+	} 
+	printf("Hello Holberton\n");
+	}
+	 return 0;
+	
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Hello Holberton\n");
+	}
+	printf("Display \n");
